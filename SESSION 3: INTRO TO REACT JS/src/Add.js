bffc5b03352a8d8b1162f0ae2965a533fb5d89a3
Chau Tran <81792751+chautran023@users.noreply.github.com>
2023-03-08 12:19:00 +0700

    Add files via upload

---
+++ SESSION 3: INTRO TO REACT JS/src/Add.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function Add({ onAddNew }) {
+  const [input, setInput] = useState({
+    name: '',
+  });
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInput((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (onAddNew && typeof onAddNew === 'function') {
+      onAddNew(input);
+      setInput({
+        name: '',
+      });
+    }
+  };
+
+  return (
+    <form className="row g-3" onSubmit={handleSubmit}>
+      <div className="col-auto">
+        <label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="To do..."
+            name="name"
+            value={input.name}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-3">
+          Add
+        </button>
+      </div>
+    </form>
+  );
+}
